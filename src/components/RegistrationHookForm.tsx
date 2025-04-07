35fca2603f0d80b4421fdf04cd07f4fc7d8cb1b2
DenisHerzhyk <denis.herzhyk88@gmail.com>
2025-04-07 23:49:58 +0200

    Changed error message for name and password fields

--- src/components/RegistrationHookForm.tsx
+++ src/components/RegistrationHookForm.tsx
@@ -12,7 +12,7 @@ const RegistrationHookForm = () => {
                         <label htmlFor="name">Enter name: </label>
                         <input className="form__field" type="text" name="name" id="name_N2" placeholder="Enter name" {...register('name',{required: true, minLength: {value: 2, message: "Name must be more than 2 characters"}, maxLength: {value: 50, message: "Name must not contain more than 50 characters"}})} autoComplete="name" />
                     </div>
-                    {errors.name && <p className="form__error">{errors.name.message}</p>}
+                    {errors.name && <p className="form__error">{errors.name.message ? errors.name.message : "Name is required"}</p>}
                 </div>
                 <div className="form__item">
                     <div className="form__field">
@@ -26,7 +26,7 @@ const RegistrationHookForm = () => {
                         <label htmlFor="password">Enter password: </label>
                         <input className="form__field" type="password" name="password" id="password_N2" placeholder="Enter password" {...register('password',{ required: true, minLength: {value: 2, message:"Password must be more than 2 characters"}})} autoComplete="current-password" />
                     </div>
-                    {errors.password && <p className="form__error">{errors.password.message}</p>}
+                    {errors.password && <p className="form__error">{errors.password.message ? errors.password.message : "Password is required"}</p>}
                 </div>
                 <button className="form__button" type="submit">Submit</button>
             </form>
