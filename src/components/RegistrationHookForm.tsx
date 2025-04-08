43738d810cd4dc99a9371e5bf6b16d92e145be1d
DenisHerzhyk <denis.herzhyk88@gmail.com>
2025-04-08 17:07:51 +0200

    Fixed exeption for field IDs

--- src/components/RegistrationHookForm.tsx
+++ src/components/RegistrationHookForm.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {Control, useForm, useWatch} from 'react-hook-form';
 import "../src/scss/RegistrationForm.scss";
 
@@ -5,32 +6,35 @@ const watchValue = (control: Control, fieldName: string) => useWatch({control, n
 
 const RegistrationHookForm = () => {
     const {register, control, handleSubmit, formState: {errors}} = useForm();
-    const nameValue = watchValue(control, "name");
-    const emailValue = watchValue(control, "email");
-    const passwordValue = watchValue(control, "password");
+    const nameValue = watchValue(control, 'name_N2');
+    const emailValue = watchValue(control, 'email_N2');
+    const passwordValue = watchValue(control, 'password_N2');
+
+    const onSubmit = (data: any) => console.log(data);
+
     return (
         <>
-            <form className="form" onSubmit={handleSubmit(onsubmit)}>
+            <form className="form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="form__item">
                     <div className="form__field">
-                        <label htmlFor="name">Enter name: </label>
-                        <input className="form__field" type="text" name="name" id="name_N2" placeholder="Enter name" {...register('name',{required: "Name is required", minLength: {value: 2, message: "Name must be more than 2 characters"}, maxLength: {value: 50, message: "Name must not contain more than 50 characters"}})} autoComplete="name" />
+                        <label htmlFor="name_N2">Enter name: </label>
+                        <input className="form__field" type="text" id="name_N2" placeholder="Enter name" {...register('name_N2',{required: "Name is required", minLength: {value: 2, message: "Name must be more than 2 characters"}, maxLength: {value: 50, message: "Name must not contain more than 50 characters"}})} autoComplete="name" />
                     </div>
-                    {errors.name ? <p className="form__error">{errors.name.message}</p> : nameValue && <p className="form__success">Correct name</p>}
+                    {errors.name_N2 ? <p className="form__error">{String(errors.name_N2.message)}</p> : nameValue && <p className="form__success">Correct name</p>}
                 </div>
                 <div className="form__item">
                     <div className="form__field">
-                        <label htmlFor="email">Enter email: </label>
-                        <input className="form__field" type="text" name="email" id="email_N2" placeholder="Enter email" {...register('email',{required: "Email is required", pattern: {value: /^[^\s@]+@[^\s@]+\.com$/i, message: "Email must contain @ sign and .com at the end"}})} autoComplete="email" />
+                        <label htmlFor="email_N2">Enter email: </label>
+                        <input className="form__field" type="text" id="email_N2" placeholder="Enter email" {...register('email_N2',{required: "Email is required", pattern: {value: /^[^\s@]+@[^\s@]+\.com$/i, message: "Email must contain @ sign and .com at the end"}})} autoComplete="email" />
                     </div>
-                    {errors.email ? <p className="form__error">{errors.email.message}</p> : emailValue && <p className="form__success">Correct email</p>}
+                    {errors.email_N2 ? <p className="form__error">{String(errors.email_N2.message)}</p> : emailValue && <p className="form__success">Correct email</p>}
                 </div>
                 <div className="form__item">
                     <div className="form__field">
-                        <label htmlFor="password">Enter password: </label>
-                        <input className="form__field" type="password" name="password" id="password_N2" placeholder="Enter password" {...register('password',{ required: "Password is required", minLength: {value: 2, message:"Password must be more than 2 characters"}})} autoComplete="current-password" />
+                        <label htmlFor="password_N2">Enter password: </label>
+                        <input className="form__field" type="password" id="password_N2" placeholder="Enter password" {...register('password_N2',{ required: "Password is required", minLength: {value: 2, message:"Password must be more than 2 characters"}})} autoComplete="current-password" />
                     </div>
-                    {errors.password ? <p className="form__error">{errors.password.message}</p> : passwordValue && <p className="form__success">Correct password</p>}
+                    {errors.password_N2 ? <p className="form__error">{String(errors.password_N2.message)}</p> : passwordValue && <p className="form__success">Correct password</p>}
                 </div>
                 <button className="form__button" type="submit">Submit</button>
             </form>
